62368bceafd9adbd9a4f1fa66823e300968b407d
Zems <100041965+zemsuk@users.noreply.github.com>
2024-06-26 14:29:36 +0600

    Update AuthController.php

--- src/AuthController.php
+++ src/AuthController.php
@@ -1,17 +1,63 @@
 <?php
 namespace Zems\LrAuth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 
-
-class AuthController extends Controller
+class AuthController extends ZemsAuth
 {     
-    public function index($data = false)
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('zems_dev')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
     {
-        return "Zems Auth demo";
-    }    
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('zems_dev')->plainTextToken; 
+            $success['name'] =  $user->name;
+   
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+    public function logout(Request $request, $id)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        // return response()->json([], 204); // No content response
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }   
     
     
 }
